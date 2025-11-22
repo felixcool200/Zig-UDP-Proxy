4b6bc8984feb36f1baa73c7fbc9499c77f816639
Felix Söderman <felixsoderman+github@gmail.com>
2025-11-22 13:04:22 +0100

    Added claude code to shell.

--- shell.nix
+++ shell.nix
@@ -1,11 +1,16 @@
 {
-  pkgs ? import <nixpkgs> { },
+  pkgs ? import <nixpkgs> {
+    config.allowUnfree = true;
+  },
 }:
 
 pkgs.mkShell {
   buildInputs = with pkgs; [
     zig
     zls
+
+    # AI tools
+    claude-code
   ];
 }
 
